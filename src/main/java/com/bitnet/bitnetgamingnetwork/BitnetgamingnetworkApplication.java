4aca8e04259dd452975736d21aa1dd1d548488cb
AlexHang <alexandruhang@yahoo.com>
2021-04-12 10:25:27 +0300

    first commit

---
+++ src/main/java/com/bitnet/bitnetgamingnetwork/BitnetgamingnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.bitnet.bitnetgamingnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitnetgamingnetworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitnetgamingnetworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitnet/bitnetgamingnetwork/BitnetgamingnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitnet.bitnetgamingnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitnetgamingnetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
